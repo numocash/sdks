05ae0a8cc454bda98476d794a7a2502e9f663efa
Kyle Scott <kyscott@umich.edu>
2023-09-25 08:57:43 -0400

    panoptic update deps

--- packages/panoptic-sdk/src/publicActions/getPanopticCollateralPositionData.ts
+++ packages/panoptic-sdk/src/publicActions/getPanopticCollateralPositionData.ts
@@ -1,4 +1,4 @@
-import { type ReverseMirage, getERC20BalanceOf } from "reverse-mirage";
+import { getERC20BalanceOf } from "reverse-mirage";
 import type {
   Address,
   Chain,
@@ -25,7 +25,6 @@ export type GetPanopticCollateralPositionDataReturnType =
 
 export const getPanopticCollateralPositionData = <
   TChain extends Chain | undefined,
-  T extends "select" | undefined,
 >(
   client: Client<Transport, TChain>,
   {
@@ -33,37 +32,9 @@ export const getPanopticCollateralPositionData = <
     address,
     ...request
   }: GetPanopticCollateralPositionDataParameters,
-  type?: T,
-) =>
-  (type === undefined
-    ? getERC20BalanceOf(client, {
-        erc20: panopticCollateral,
-        address,
-        ...request,
-      })
-    : {
-        read: () =>
-          getERC20BalanceOf(
-            client,
-            {
-              erc20: panopticCollateral,
-              address,
-              ...request,
-            },
-            "select",
-          ).read(),
-        parse: (data) =>
-          getERC20BalanceOf(
-            client,
-            {
-              erc20: panopticCollateral,
-              address,
-              ...request,
-            },
-            "select",
-          ).parse(data),
-      }) as ReverseMirage<
-    bigint,
-    GetPanopticCollateralPositionDataReturnType,
-    T
-  >;
+): Promise<GetPanopticCollateralPositionDataReturnType> =>
+  getERC20BalanceOf(client, {
+    erc20: panopticCollateral,
+    address,
+    ...request,
+  });
--- packages/panoptic-sdk/src/publicActions/getPanopticPoolData.ts
+++ packages/panoptic-sdk/src/publicActions/getPanopticPoolData.ts
@@ -1,4 +1,3 @@
-import { type ReverseMirage } from "reverse-mirage";
 import { getUniswapV3PoolData } from "uniswap-v3-sdk";
 import type { Chain, Client, ReadContractParameters, Transport } from "viem";
 import { collateralTrackerABI } from "../generated.js";
@@ -14,125 +13,27 @@ export type GetPanopticPoolDataParameters = Omit<
 
 export type GetPanopticPoolDataReturnType = PanopticPoolData;
 
-export const getPanopticPoolData = <
-  TChain extends Chain | undefined,
-  T extends "select" | undefined,
->(
+export const getPanopticPoolData = <TChain extends Chain | undefined,>(
   client: Client<Transport, TChain>,
   { panopticPool, ...request }: GetPanopticPoolDataParameters,
-  type?: T,
-) =>
-  (type === undefined
-    ? Promise.all([
-        getUniswapV3PoolData(client, {
-          pool: panopticPool.uniswapPool,
-          ...request,
-        }),
-        getPanopticCollateralData(client, {
-          panopticCollateral: panopticPool.collateralTracker0,
-          ...request,
-        }),
-        getPanopticCollateralData(client, {
-          panopticCollateral: panopticPool.collateralTracker1,
-          ...request,
-        }),
-      ]).then(([poolData, collateral0Data, collateral1Data]) => ({
-        type: "panopticPoolData",
-        panopticPool,
-        collateralTracker0Data: collateral0Data,
-        collateralTracker1Data: collateral1Data,
-        uniswapPoolData: poolData,
-      }))
-    : {
-        read: () =>
-          Promise.all([
-            getUniswapV3PoolData(
-              client,
-              {
-                pool: panopticPool.uniswapPool,
-                ...request,
-              },
-              "select",
-            ).read(),
-            getPanopticCollateralData(
-              client,
-              {
-                panopticCollateral: panopticPool.collateralTracker0,
-                ...request,
-              },
-              "select",
-            ).read(),
-            getPanopticCollateralData(
-              client,
-              {
-                panopticCollateral: panopticPool.collateralTracker1,
-                ...request,
-              },
-              "select",
-            ).read(),
-          ]),
-        parse: (data) => ({
-          type: "panopticPoolData",
-          uniswapPoolData: getUniswapV3PoolData(
-            client,
-            {
-              pool: panopticPool.uniswapPool,
-              ...request,
-            },
-            "select",
-          ).parse(data[0]),
-          collateralTracker0Data: getPanopticCollateralData(
-            client,
-            {
-              panopticCollateral: panopticPool.collateralTracker0,
-              ...request,
-            },
-            "select",
-          ).parse(data[1]),
-          collateralTracker1Data: getPanopticCollateralData(
-            client,
-            {
-              panopticCollateral: panopticPool.collateralTracker1,
-              ...request,
-            },
-            "select",
-          ).parse(data[1]),
-        }),
-      }) as ReverseMirage<
-    [
-      readonly [
-        {
-          poolData: readonly [
-            bigint,
-            number,
-            number,
-            number,
-            number,
-            number,
-            boolean,
-          ];
-          tickData: readonly [
-            readonly [
-              bigint,
-              bigint,
-              bigint,
-              bigint,
-              bigint,
-              bigint,
-              number,
-              boolean,
-            ],
-            bigint,
-          ];
-        },
-        bigint,
-        bigint,
-        bigint,
-        readonly [bigint, bigint],
-      ],
-      [bigint, [bigint, bigint, bigint]],
-      [bigint, [bigint, bigint, bigint]],
-    ],
-    GetPanopticPoolDataReturnType,
-    T
-  >;
+): Promise<GetPanopticPoolDataReturnType> =>
+  Promise.all([
+    getUniswapV3PoolData(client, {
+      pool: panopticPool.uniswapPool,
+      ...request,
+    }),
+    getPanopticCollateralData(client, {
+      panopticCollateral: panopticPool.collateralTracker0,
+      ...request,
+    }),
+    getPanopticCollateralData(client, {
+      panopticCollateral: panopticPool.collateralTracker1,
+      ...request,
+    }),
+  ]).then(([poolData, collateral0Data, collateral1Data]) => ({
+    type: "panopticPoolData",
+    panopticPool,
+    collateralTracker0Data: collateral0Data,
+    collateralTracker1Data: collateral1Data,
+    uniswapPoolData: poolData,
+  }));
