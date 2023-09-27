ae00f84eb13c4959a1d56686dda68e4af12f59f1
Kyle Scott <kyscott@umich.edu>
2023-09-27 10:56:28 -0400

    don't use guillaume's private alchemy api key

--- packages/panoptic-sdk/src/_test/globalSetup.ts
+++ packages/panoptic-sdk/src/_test/globalSetup.ts
@@ -4,8 +4,7 @@ import { testClient } from "./utils.js";
 export default async function () {
   const shutdown = await startProxy({
     options: {
-      forkUrl:
-        "https://eth-sepolia.g.alchemy.com/v2/kw-ycolm1dq00fqT_Y-b-FkV-Ai6h38m",
+      forkUrl: "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
     },
   });
 
